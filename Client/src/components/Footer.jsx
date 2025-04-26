e8db5df2f5b0110a4558ac65870a4bad75be5164
bishal292 <bs9107709@gmail.com>
2025-04-27 02:29:33 +0530

    Final commit -Added Footer and other relevant improvements.

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="mt-3 flex flex-col items-center justify-center bg-gray-800 text-white text-center py-4">
-      <p>Made with ❤️ by <a href="https://github.com/bishal292" target="_blank"><span className="text-blue-300 hover:text-red-600"> @Bishal Singh</span></a> </p>
+    <footer className="w-full z-50 mt-3 flex flex-col items-center justify-center bg-gray-200 text-gray-700 text-center py-4">
+      <p>Made with ❤️ by <a href="https://github.com/bishal292" target="_blank"><span className="text-blue-500 hover:text-red-600"> @Bishal Singh</span></a> </p>
       <p>&copy; {new Date().getFullYear()} Waste Management System. All rights reserved.</p>
     </footer>
   );
